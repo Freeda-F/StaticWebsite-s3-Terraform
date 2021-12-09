9e2d7a410a4f72d49c9b0b9ad5b2e9247a754bdd
Freeda Francis <93197553+Freeda-F@users.noreply.github.com>
2021-12-09 20:35:46 +0530

    variables.tf

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "bucket-name" {
+ default = "s3-demo1.freeda-francis.tech"
+}
+
+variable "location" {
+    default = "website"
+}
+
+variable "region" {
+  default = "us-east-2"
+}
+
+variable "access_key" {
+  default = "Enter access-key here"
+}
+
+variable "secret_key" {
+  default = "Enter secret-key here"
+}
